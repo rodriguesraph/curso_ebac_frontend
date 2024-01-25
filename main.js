a76f7524dae4c1ff5616e4bbbffbe5e5b1305155
Raphael Rodrigues <raphaelrodrigues_flk@hotmail.com>
2024-01-24 21:54:59 -0300

    Fix: Padronizando casas decimais com .toFixed(2)

--- main.js
+++ main.js
@@ -58,6 +58,6 @@ function calcMedia() {
 function attMedia() {
     const mediaFinal = calcMedia();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
