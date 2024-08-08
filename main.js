0cba656ec27e8ccae924a56f2aac7c6a41fe2d1c
Raphael Rodrigues <raphaelrodrigues_flk@hotmail.com>
2024-08-08 20:57:32 -0300

    finalizando exercicio

---
+++ main.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nameFetch = document.querySelector('#name');
+    const usernameFetch = document.querySelector('#username');
+    const avatarFetch = document.querySelector('#avatar');
+    const reposFetch = document.querySelector('#repos');
+    const followersFetch = document.querySelector('#followers');
+    const followingFetch = document.querySelector('#following');
+    const linkFetch = document.querySelector('#link');
+
+    fetch('https://api.github.com/users/rodriguesraph').then(function(res) {
+        return res.json();
+    })
+    .then(function(json) {
+        nameFetch.innerText = json.name;
+        usernameFetch.innerText = json.login;
+        avatarFetch.src = json.avatar_url;
+        followersFetch.innerText = json.followers;
+        followingFetch.innerText = json.following;
+        reposFetch.innerText = json.public_repos;
+        linkFetch.href = json.html_url;
+    })
+})
